f89e218624f8f7e15cd7e7387bd37927fe7e67f9
jhonex1234 <jajoya17@misena.edu.co>
2019-08-25 15:24:56 -0500

    add template folder

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, Response, request, redirect, url_for, jsonify
 from flask import session, flash
 
-app = Flask(__name__) #, template_folder="com_projectFlores_template")
+app = Flask(__name__, template_folder="ugestor_template")
 
 @app.route('/page1', methods=['GET', 'POST'])
 def index():
