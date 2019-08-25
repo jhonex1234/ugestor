4ef172f1103ae3c5d4b3f4168bc05eb06f7b1b7d
jhonex1234 <jajoya17@misena.edu.co>
2019-08-25 15:05:27 -0500

    add server file

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, Response, request, redirect, url_for, jsonify
+from flask import session, flash
+
+app = Flask(__name__) #, template_folder="com_projectFlores_template")
+
+@app.route('/page1', methods=['GET', 'POST'])
+def index():
+    return "Hola mundo, here"#render_template("404.html")
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True, threaded=True) #, host='0.0.0.0'
