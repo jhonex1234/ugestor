3963cb15b20246f17a4e89d3af4d8e4114908dd2
jhonex1234 <jajoya17@misena.edu.co>
2019-08-25 16:09:26 -0500

    nuevo cambios

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__, template_folder="ugestor_template")
 
 @app.route('/page1', methods=['GET', 'POST'])
 def index():
-    return "Hola mundo, here"#render_template("404.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True, threaded=True) #, host='0.0.0.0'
