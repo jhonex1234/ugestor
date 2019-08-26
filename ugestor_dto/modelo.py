0f18867696d40c14fba785071818254354e02a08
felipe <efnaranjo>
2019-08-25 22:30:40 -0500

    actualizacion

--- ugestor_dto/modelo.py
+++ ugestor_dto/modelo.py
@@ -1,5 +1,16 @@
 from Persona import Persona
 
 
-def __init__(self, ):
-	super().__init__(self, id, cedula, nombre, apellido, fechaNacimiento, sexo)
+class Modelo:
+def __init__(self, id, colorOjos, colorPiel, estatura, medidaCintura,medidaBusto, tallaZapatos, peso, particularidades):
+	super().__init__(self, id, cedula, cedula, apellido, fechaNacimiento, sexo)
+	self.__id = id
+	self.__colorOjos = colorOjos
+	self.__colorPiel
+	self.__estatura
+	self.__medidaCintura
+	self.__medidaBusto
+	self.__medidaBusto
+	self.__tallaZapatos
+	self.__peso
+	self.__particularidades
